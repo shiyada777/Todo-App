a3b26c2e2491b04426ce48a13885ca21f0f2ce6c
shiyada777 <shivakmr2025@gmail.com>
2025-11-20 21:31:11 -0600

    making the add task function workable phase 1

---
+++ script.js
@@ -0,0 +1,26 @@
+// Taken all the variables 
+
+let inputBox=document.getElementById("inputBox");
+let addTaskButton=document.getElementById("addTaskButton");
+let orderList=document.getElementById("orderList");
+
+
+
+//add task function
+addTaskButton.addEventListener("click",function(){
+    if(inputBox.value===""){
+        console.log("return")
+    }
+    let task=inputBox.value.trim();
+    let div=document.createElement("div");
+    div.innerHTML=`
+    <input type="checkbox" name="" id="checkBox">
+    <p id="taskText">${task}</p>
+     <p id="date">Date</p>
+    <button id="editTaskButton">Edit</button>
+    <button id="deleteTaskButton">Delete</button>`
+   
+    div.classList.add("elementCreated")
+    taskLists.append(div)
+    inputBox.value="";
+});
